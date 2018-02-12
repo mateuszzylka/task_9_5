4132c66d828b0b716cafe06429f303db99c06231
Mateusz Zylka <mateuszzylkaa@gmail.com>
2018-02-12 21:43:42 +0100

    Change in js file

--- scripts.js
+++ scripts.js
@@ -2,4 +2,7 @@ var withButtonsClass = document.getElementsByClassName('button');
 
 for (var i = 0; i < withButtonsClass.length; i++) {
 	alert(withButtonsClass[i].innerText);
+	var numButtons = withButtonsClass.length;
 }
+
+alert('Number of buttons : ' + numButtons);
